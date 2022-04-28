a023b001046af8f8a39954d6be318e76892ebc2f
Seyfullah <seyfullah.akgn@gmail.com>
2022-04-28 18:34:35 +0300

    first commit

---
+++ app/src/main/java/com/seyf/debugging/MainActivity.kt
@@ -0,0 +1,40 @@
+package com.seyf.debugging
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.widget.TextView
+
+private const val TAG = "MainActivity"
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val helloTextView: TextView = findViewById(R.id.hello_world)
+        helloTextView.text = "Hello Debugging!"
+        logging()
+        division()
+    }
+
+
+
+    fun logging() {
+        Log.e(TAG, "ERROR: a serious error like an app crash")
+        Log.w(TAG, "WARN: warns about the potential for serious errors")
+        Log.i(TAG, "INFO: reporting technical information, such as an operation succeeding")
+        Log.d(TAG, "DEBUG: reporting technical information useful for debugging")
+        Log.v(TAG, "VERBOSE: more verbose than DEBUG logs")
+    }
+
+    fun division() {
+        val numerator = 60
+        var denominator = 4
+        repeat(5) {
+            Thread.sleep(1)
+            Log.v(TAG, "${numerator / denominator}")
+            denominator--
+        }
+    }
+}
